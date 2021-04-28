d24f9d932b8d3978d38ed8e4d173545c8704de33
XenioxYT <tomlockwood135@outlook.vom>
2021-04-28 21:51:19 +0100

    Added Dice images, added code so the app meets descripton (and ofc comments)

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Switch
 import android.widget.TextView
 
@@ -11,23 +12,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener { rollDice() } //this is test
+        rollButton.setOnClickListener { rollDice() }
+        rollDice() // Dice roll when app starts
 
     }
     private fun rollDice() {
         val dice = Dice(numSides = 6) // makes var dice from original class Dice
-        val dice2 = Dice(numSides = 6) //makes var Dice2 from original class Dice
-        val diceRoll = dice.roll() //makes diceRoll2 from Dice function
-        val diceRoll2 = dice2.roll() //makes diceRoll2 from Dice function
-        val resultTextView: TextView = findViewById(R.id.firstDice) //target change in textView to first dice.
-        resultTextView.text = diceRoll.toString() //pushes the change in text to var diceRoll and converts to string
-        val resultTextView2: TextView = findViewById(R.id.secondDice/* specifies the textView that changes*/) //target change in textView to second dice.
-        resultTextView2.text = diceRoll2.toString() // pushes the change in text to var diceRoll and converts to string
-
+        val diceRoll = dice.roll() // Rolls dice from roll() function in Class dice and then sets diceRoll var to the value
+        val diceImage: ImageView = findViewById(R.id.imageView) // Sets diceImage to the correct I.D for the imageView
+        val diceNumber = when (diceRoll) { // on dice roll {number} store the correct image in diceNumber when the dice is rolled
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6 // when has to be exhaustive
+        }
+        diceImage.setImageResource(diceNumber) // set the imageView to the diceNumber (the number rolled and the correct image)
+        diceImage.contentDescription = diceRoll.toString() // Sets the content description of the image to the correct number
     }
 }
-class Dice(private val numSides: Int) {
-    fun roll(): Int {
-        return (1..numSides).random()
+class Dice(private val numSides: Int) { //creates private class Dice and declares the numSides as an integer
+    fun roll(): Int { // creates function roll()
+        return (1..numSides).random() //generated the random number for the dice depending on how many sides the dice has
     }
 }
